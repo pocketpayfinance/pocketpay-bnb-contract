713c375dee3354f02203179feb91877687239439
Goraniya Karan <karangoraniya@gmail.com>
2023-09-05 18:11:24 +0530

    chore : add .env example & opBNB Testnet network config 🚀

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const { PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    opbnb_testnet: {
+      url: `https://opbnb-testnet-rpc.bnbchain.org`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
